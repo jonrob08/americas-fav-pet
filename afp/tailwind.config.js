8165ed207724f7b301d85f2676b8582f0faacf40
jonrob <jonrob08@gmail.com>
2023-02-04 11:49:06 -0700

    chore: created nuxt 3 app, added in tailwind css

---
+++ afp/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./nuxt.config.{js,ts}",
+  ],
+  darkMode: 'class',
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
